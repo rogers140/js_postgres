0264505fd31509cff5b104e27604fedd0d537aa2
rogers140 <rogers140@gmail.com>
2017-01-17 21:39:07 -0800

    Test on simple postgres database

---
+++ postgresBackend.js
@@ -0,0 +1,19 @@
+var Q = require('q');
+var pg_promise = require('pg-promise')({promiseLib: Q});
+var connection = {
+	host: 'localhost',
+	port: '5432',
+	database: 'postgres',
+	user: 'postgres',
+	password: ''
+};
+var db = pg_promise(connection);
+console.log('Database created.');
+db.any("select * from users", [true])
+	.then(function(data) {
+		console.log(data);
+	})
+	.catch(function(error) {
+		console.log(erro);
+	})
+	.done();
